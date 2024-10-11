c5919990ec8c7e66e3dd8764e6663dde8d3c5a97
Shobhit Tyagi <shobhittyagimit@gmail.com>
2024-10-11 05:13:30 +0000

    ui changes

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -10,38 +10,21 @@ sap.ui.define([
 
         return Controller.extend("com.sap.supplier.controller.View1", {
             onInit: function () {
-                let data = [
-                    {
-                        "supplierid" : "SUP1001",
-                        "vendorid": "VEN2001",
-                        "acmid" : "ACM0001",
-                        "erpid" : "ERP3001"
-                    },
-                    {
-                        "supplierid" : "SUP1002",
-                        "vendorid": "VEN2002",
-                        "acmid" : "ACM0002",
-                        "erpid" : "ERP3002"
-                    },
-                    {
-                        "supplierid" : "SUP1003",
-                        "vendorid": "VEN2003",
-                        "acmid" : "ACM0003",
-                        "erpid" : "ERP3003"
-                    },
-                    {
-                        "supplierid" : "SUP1004",
-                        "vendorid": "VEN2004",
-                        "acmid" : "ACM0004",
-                        "erpid" : "ERP3004"
-                    }
-                ];
-                let omodel = new JSONModel(data);
-                this.getView().setModel(omodel, "myModel")
 
             },
-            onTableRowPress:function(){
-                this.getOwnerComponent().getRouter().navTo("RouteView2");
+            onTableRowPress: function (oEvent) {
+                let oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+
+
+                let oSelectedItem = oEvent.getSource();
+                let oBindingContext = oSelectedItem.getBindingContext();
+                let sID = oBindingContext.getProperty("ID");
+
+
+                oRouter.navTo("RouteView2", {
+                    ID: sID
+                });
             }
+
         });
     });
--- webapp/controller/View2.controller.js
+++ webapp/controller/View2.controller.js
@@ -1,52 +1,34 @@
 sap.ui.define(
     [
-        "sap/ui/core/mvc/Controller"
+        "sap/ui/core/mvc/Controller",
+        "sap/ui/model/json/JSONModel",
+        "sap/ui/core/UIComponent"
     ],
-    function(BaseController) {
-      "use strict";
-  
-      return BaseController.extend("com.sap.supplier.controller.View2", {
-        onInit: function() {
-        },
-        itemValidationCallback: function (oItemInfo) {
-            const { oItem, iTotalItemsForUpload } = oItemInfo;
-            var oUploadSetTableInstance = this.byId("UploadSetTable");
-            var oSelectedItems = oUploadSetTableInstance.getSelectedItems();
-            var oSelectedItemForUpdate = oSelectedItems.length === 1 ? oSelectedItems[0] : null;
-            if (oSelectedItemForUpdate && oSelectedItemForUpdate.getFileName() === "-" && iTotalItemsForUpload === 1) {
-                return new Promise((resolve) => {
-                    if (oSelectedItemForUpdate) {
-                        var oContext = oSelectedItemForUpdate.getBindingContext();
-                        var data = oContext && oContext.getObject ? oContext.getObject() : {};
+    function (BaseController, JSONModel, UIComponent) {
+        "use strict";
 
-                        /* Demonstration use case of Setting the header field if required to be passed in API request headers to
-                                inform backend with document type captured through user input */
-                        oItem.addHeaderField(new CoreItem(
-                            {
-                                key: "existingDocumentID",
-                                text: data ? data.id : ""
-                            }
-                        ));
-                    }
-                    resolve(oItem);
-                });
-            } else {
-                var oItemPromise = new Promise((resolve, reject) => {
-                    this.oItemsProcessor.push({
-                        item: oItem,
-                        resolve: resolve,
-                        reject: reject
-                    });
-                });
-                if (iTotalItemsForUpload === 1) {
-                    this.openFileUploadDialog();
-                } else if (iTotalItemsForUpload === this.oItemsProcessor.length) {
-                    this.openFileUploadDialog();
-                }
-                return oItemPromise;
-            }
-        },
-      });
+        return BaseController.extend("com.sap.supplier.controller.View2", {
+            onInit: function () {
+                let oRouter = UIComponent.getRouterFor(this);
+                oRouter
+                    .getRoute("RouteView2")
+                    .attachPatternMatched(this._getMatchedRoute, this);
+
+
+
+
+            },
+
+            _getMatchedRoute: function (oEvent) {
+                debugger
+                let ID = oEvent.getParameters().arguments.ID;
+                let sPath = `/supplierProfileT('${ID}')`;
+                this.getView().bindElement(sPath);
+                
+              }
+
+
+
+        });
     }
-  );
-  
+);
